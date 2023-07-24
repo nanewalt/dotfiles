c0458de0d50327dbed16347994e832d0376cdc79
nanewalt <55122681+nanewalt@users.noreply.github.com>
2023-07-24 13:54:30 -0400

    Add oh-my-zsh & powerlevel10k

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 echo "> Installing dot files..."
 
 brew install zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 source ~/.zshrc
 source ~/.vimrc
