040ea36bebe702d9430a819aa07e996219cdbf19
nanewalt <55122681+nanewalt@users.noreply.github.com>
2023-07-24 14:51:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 echo "> Installing dot files..."
 
+# Install brew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+# Install zsh, oh-my-zsh, and p10k
 brew install zsh
+brew install zsh-syntax-highlighting
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-source ~/.zshrc
-source ~/.vimrc
 
 echo "> Installation complete!"
